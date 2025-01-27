3f65f26b35fba98a2984031bedd8fd80931aac99
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2025-01-27 16:48:49 +0000

    Added navlink and routing to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className={styles.appContainer}>
       <NavBar />
+      <Container fluid className={styles.main}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from "../styles/NavBar.css";  
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import styles from "../styles/NavBar.module.css";  
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top" >
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="/" aria-label="Home">
-          <span className="sr-only">Home</span>
-        </Navbar.Brand>
+        <NavLink to="/" className="navbar-brand">
+          <Navbar.Brand aria-label="Home">
+            <span className="sr-only">Home</span>
+          </Navbar.Brand>
+        </NavLink>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto text-left">
-            <Nav.Link href="/" aria-label="Home">
+          <Nav className="ml-auto text-left">
+            <Nav.Link 
+              as={NavLink} 
+              to="/" 
+              aria-label="Home" 
+              className={({ isActive }) => (isActive ? styles.activeLink : '')}
+            >
               <i className="fas fa-home"></i> Home
             </Nav.Link>
-            <Nav.Link href="/signin" aria-label="Sign in">
+            <Nav.Link 
+              as={NavLink} 
+              to="/signin" 
+              aria-label="Sign in" 
+              className={({ isActive }) => (isActive ? styles.activeLink : '')}
+            >
               <i className="fas fa-sign-in-alt"></i> Sign in
             </Nav.Link>
-            <Nav.Link href="/signup" aria-label="Sign up">
+            <Nav.Link 
+              as={NavLink} 
+              to="/signup" 
+              aria-label="Sign up" 
+              className={({ isActive }) => (isActive ? styles.activeLink : '')}
+            >
               <i className="fas fa-user-plus"></i> Sign up
             </Nav.Link>
           </Nav>
