322b4ad5a1e1184f4ebd45072cfdd7cc26150e91
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-24 23:20:11 +0000

    Add function to fetch a post

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom";
 
 
 function PostPage() {
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const [{ data: post }, { data: comments }] = await Promise.all([
+              axiosReq.get(`/posts/${id}`),
+            ]);
+            setPost({ results: [post] });
+          } catch (err) {
+          }
+        };
+    
+    
+        handleMount();
+      }, [id]);
     
   
   
