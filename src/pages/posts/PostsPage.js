c8a497767ccf0e1bf7ffe4b3bcd5b439cd4e83a4
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-26 23:41:17 +0000

    Added list of post and filter to the home page

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -10,21 +10,65 @@ import Asset from "../../components/Asset";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
+import NoResults from "../../assets/no-results.png";
 
-function PostsPage() {
+function PostsPage({ message, filter = "" }) {
+    const [posts, setPosts] = useState ({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+  
+    const [query, setQuery] = useState("");
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          try {
+            const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+            setPosts(data);
+            setHasLoaded(true);
+          } catch (err) {
+          }
+        };
+
+        setHasLoaded(false);
+        const timer = setTimeout(() => {
+        fetchPosts();
+        }, 1000);
+
+        return () => {
+        clearTimeout(timer);
+        };
+    }, [filter, query, pathname]);
   
     return (
-      <Row className="h-100">
-        <Col className="py-2 p-0 p-lg-2" lg={8}>
-          <p>Popular profiles mobile</p>
-          <p>List of posts here</p>
-        </Col>
-        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-          <p>Popular profiles for desktop</p>
-        </Col>
-      </Row>
-    );
-  }
-  
-  export default PostsPage;
+        <Row className="h-100">
+          <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <p>Popular profiles mobile</p>
+            {hasLoaded ? (
+              <>
+                {posts.results.length ? (
+                  posts.results.map((post) => (
+                    <Post key={post.id} {...post} setPosts={setPosts} />
+                  ))
+                ) : (
+                  <Container className={appStyles.Content}>
+                    <Asset src={NoResults} message={message} />
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset spinner />
+              </Container>
+            )}
+          </Col>
+          <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+            <p>Popular profiles for desktop</p>
+          </Col>
+        </Row>
+      );
+    }
+    
+    export default PostsPage;
