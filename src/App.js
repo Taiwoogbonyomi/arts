a86d25e7f2126651c8680a0ea29f265157417938
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-26 23:01:13 +0000

    Update the route for the homepage and add timeline and likes route

--- src/App.js
+++ src/App.js
@@ -7,14 +7,41 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
 
 function App() {
+
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route 
+            exact path="/" 
+            render={() => <PostsPage message="Sorry, no results match your search. Try another keyword."/>} 
+          />
+          <Route
+            exact path="/Timeline"
+            render={() => (
+              <PostsPage
+                message="Sorry, no results were found. Try searching a different keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="You haven't liked anything with that keyword. Try searching a different term or liking a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/create-post" render={() => <PostCreateForm />} />
