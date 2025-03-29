65da8e9423e9bb023c3ad5a03d8b665d5ad14ba2
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-29 23:18:30 +0000

    Add moredropdown menu and functionality to delete comment

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,34 @@ import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
 import styles from "../../styles/Comment.module.css"
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { axiosRes } from '../../api/axiosDefaults';
 
 const Comment = (props) => {
-    const { profile_id, profile_image, owner, updated_at, content } = props;
+    const { profile_id, profile_image, owner, updated_at, content, id, setPost, setComments} = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+          await axiosRes.delete(`/comments/${id}/`);
+          setPost((prevPost) => ({
+            results: [
+              {
+                ...prevPost.results[0],
+                comments_count: prevPost.results[0].comments_count - 1,
+              },
+            ],
+          }));
+    
+          setComments((prevComments) => ({
+            ...prevComments,
+            results: prevComments.results.filter((comment) => comment.id !== id),
+          }));
+        } catch (err) {}
+      };
 
     return (
         <div>
@@ -16,9 +41,12 @@ const Comment = (props) => {
             </Link>
             <Media.Body className="align-self-center ml-2">
               <span className={styles.Owner}>{owner}</span>
-              <span className={styles.Date}>{updated_at}</span>
+              <span className={styles.Date}>{updated_at?.humanized || "Unknown date"}</span>
               <p>{content}</p>
             </Media.Body>
+            {is_owner && (
+                <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+            )}
           </Media>
         </div>
       );
