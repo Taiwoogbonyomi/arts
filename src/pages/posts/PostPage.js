1ef55e3507b3694979e56fa09d8be96adb9e1d94
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-29 22:00:38 +0000

    Add CommentCreateForm to Postpage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,7 +8,7 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
-import CommentCreateForm from "../comments/CommentCreateForm";
+import CommentCreateForm from "../comments/CommentCreateForm"
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
@@ -17,7 +17,7 @@ function PostPage() {
 
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
-  const [comments, setComments] = useState({ results: [] });
+  const [setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -38,7 +38,15 @@ function PostPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage={true} />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          <CommentCreateForm 
+            profile_id={currentUser.profile_id}
+            profileImage={profile_image}
+            post={id}
+            setPost={setPost}
+            setComments={setComments}
+          />
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
