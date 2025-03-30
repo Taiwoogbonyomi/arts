3912a57638af34224cc1c63955e49b1624080bed
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-30 22:29:47 +0100

    Add the ProfileDataProvider around the app component in index file

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 ReactDOM.render(
   <React.StrictMode>
      <BrowserRouter>
-         <CurrentUserProvider>
-            <App />
-         </CurrentUserProvider>
+      <CurrentUserProvider>
+      <ProfileDataProvider>
+      <App />
+      </ProfileDataProvider>
+      </CurrentUserProvider>
      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
