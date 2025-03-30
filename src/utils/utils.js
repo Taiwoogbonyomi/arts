9b875d914407c701b5d9a00ba0b80d7033cf51f0
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-31 00:49:50 +0100

    Add unfollow function in utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -34,6 +34,21 @@ export const followHelper = (profile, clickedProfile, following_id) => {
       profile;
 };
 
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? 
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? 
+      { ...profile, following_count: profile.following_count - 1 }
+    : 
+      profile;
+};
+
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
   localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
